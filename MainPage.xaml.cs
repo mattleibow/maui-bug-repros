64b73825f99c8bd61095e3abcffe1b657fa91d6d
Matthew Leibowitz <mattleibow@live.com>
2022-12-14 00:57:52 +0200

    add code

---
+++ MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace TestMaui;
+
+public partial class MainPage : ContentPage
+{
+    private string name = "This is text.";
+
+    public MainPage()
+    {
+        InitializeComponent();
+
+        BindingContext = this;
+    }
+
+    public string Name
+    {
+        get => name;
+        set
+        {
+            System.Diagnostics.Debug.WriteLine($"Name fired: '{name}' => '{value}'");
+
+            name = value;
+        }
+    }
+}
+
+
