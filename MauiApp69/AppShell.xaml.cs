87ab01d3659527788faa90b8852320f624990eb8
Matthew Leibowitz <mattleibow@live.com>
2023-01-17 01:38:36 +0200

    Added the sample

---
+++ MauiApp69/AppShell.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace MauiApp69;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ MauiApp69/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+
+namespace MauiApp69;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+
+		//Content = new MyView()
+		//{
+		//	new Label()
+		//};
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+
+public class MyView : View, IEnumerable
+{
+IEnumerator IEnumerable.GetEnumerator()
+{
+	yield return null;
+}
+}
